0661a7b726e31a4058c43c2fc8db0e9e47293b24
stefifm <bruerastefania@gmail.com>
2023-05-11 21:38:04 -0300

    Some corrections...

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import jsonServer from 'json-server'
-
+const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(process.env.PORT, () => {
   console.log('JSON Server is running')
 })
